908e61687d6a18b2b720b10b26068f48266f97b4
devawa <brian.kam.nd@gmail.com>
2016-07-29 20:01:29 +0200

    First Upload

--- Revent/Resources/Resource.Designer.cs
+++ Revent/Resources/Resource.Designer.cs
@@ -31,6 +31,18 @@ namespace Revent
 		public partial class Attribute
 		{
 			
+			// aapt resource value: 0x7f010001
+			public const int civ_border_color = 2130771969;
+			
+			// aapt resource value: 0x7f010002
+			public const int civ_border_overlay = 2130771970;
+			
+			// aapt resource value: 0x7f010000
+			public const int civ_border_width = 2130771968;
+			
+			// aapt resource value: 0x7f010003
+			public const int civ_fill_color = 2130771971;
+			
 			static Attribute()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -104,6 +116,37 @@ namespace Revent
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Styleable
+		{
+			
+			public static int[] CircleImageView = new int[] {
+					2130771968,
+					2130771969,
+					2130771970,
+					2130771971};
+			
+			// aapt resource value: 1
+			public const int CircleImageView_civ_border_color = 1;
+			
+			// aapt resource value: 2
+			public const int CircleImageView_civ_border_overlay = 2;
+			
+			// aapt resource value: 0
+			public const int CircleImageView_civ_border_width = 0;
+			
+			// aapt resource value: 3
+			public const int CircleImageView_civ_fill_color = 3;
+			
+			static Styleable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Styleable()
 			{
 			}
 		}
